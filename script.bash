cc82b92f82d58bd65f09a511d91b7f7b0a580ff2
Sahm Samarghandi <sahm.samarghandi@bbot.menu>
2021-05-12 21:24:21 -0700

    pushing first script

---
+++ script.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install landscape-client
+
+
+name=$(getent passwd $USER | cut -d : -f5 | cut -d, -f1)
+sudo rm /etc/landscape/client.conf
+
+sudo tee -a /etc/landscape/client.conf > /dev/null <<EOT
+[client]
+log_level = info
+url = https://landscape.canonical.com/message-system
+ping_url = http://landscape.canonical.com/ping
+data_path = /var/lib/landscape/client
+account_name = apbbotmenu
+registration_key = asdjfhasdfuyweeuw26521367sadjw328723dsfas
+computer_title = ${name}
+script_users = ALL
+include_manager_plugins = ScriptExecution
+access_group = employees
+EOT
